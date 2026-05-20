93d9adc9c14a3d254de0d3ec93910c47a26d52a9
theonlydovud <dcubichacks2000@gmail.com>
2026-05-20 19:10:44 +0500

    Add files via upload

---
+++ Новая папка/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        libre: ['"Libre Franklin"', 'sans-serif'],
+        tenor: ['"Tenor Sans"', 'sans-serif'],
+      },
+    },
+  },
+
+  plugins: [],
+}
